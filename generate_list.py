461f6d491efee7d54d9513f5f94513f91cfa7c04
amwsuhje <55229544+amwsuhje@users.noreply.github.com>
2025-07-06 16:51:17 +0800

    Update generate_list.py

--- generate_list.py
+++ generate_list.py
@@ -4,4 +4,4 @@ def write_proxy_list(ip_list):
     with open("proxyip.txt", "w") as f:
         for ip in sorted(ip_list):
             cc = get_country(ip)
-            f.write(f"{cc} | {ip}\n")
+            f.write(f"{cc}\n{ip}\n")
